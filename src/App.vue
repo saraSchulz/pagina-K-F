ad9a1be7a17103b05c1f73d13ac766bbde692f43
saraSchulz-art <sara.ifc2024@gmail.com>
2026-03-10 15:43:16 -0300

    continuacao

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,35 @@
   </h2>
   <h3>june, 23rd 2026</h3>
 </section>
+<section class="intro">
+  <p>
+    With love and gratitude, we invite you to share in the joy of our wedding day.
+  </p>
+  <p>
+    Tuesday, june, 2026 The Golden Elm Manor, St. Augustine, New York
+  </p>
+</section>
+<section class="cards">
+  <div class="story">
+    <h3>Our Story</h3>
+  </div>
+  <div class="detals">
+    <h3>The Detals</h3>
+  </div>
+  <div class="rspv">
+    <h3>RSPV</h3>
+  </div>
+  <div class="regitry">
+    <h3>Registry</h3>
+  </div>
+</section>
+<section class="registry">
+  <h3>Registry</h3>
+  <p>
+    Your presence is the most cherished gift. Should you wish to contribute, we've created a honeymoon registry to help us embark on a memorable adventure to the Amalfi Coast.
+  </p>
+  <button>GO to Registry</button>
+</section>
 </template>
 
 <style scoped>
@@ -71,7 +100,79 @@ header{
     color: #fff;
     font-size: 25px;
     font-weight: normal;
-    margin: 0;
+    margin: 0 0 60px 0;
+  }
+}
+.intro{
+  background: url(/public/flowers.png ) no-repeat center center/cover;
+  min-height: 800px;
+  align-items: center;
+  display: flex ;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  & p{
+    font-size: 45px;
+    color: #fff;
+    text-shadow: 0 0 30px #ffffffaf;
+    width: 50%;
+  }
+}
+.cards{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  & div{
+    width: 35%;
+    padding: 300px 0;
+    margin: 50px;
+    box-shadow: 0 0 75px rgba(0, 0, 0, 0.705);
+     & h3{
+      color: #fff;
+      text-shadow: 0 0 30px #ffffffaf;
+      text-decoration: underline 1.5px;
+      font-size: 45px;
+      font-weight: normal;
+     }
+
+  }
+  & .story{
+    background: url(/public/folha.png) no-repeat center center/cover;
+
+  }
+  & .detals{
+    background: url(/public/carta.png) no-repeat center center/cover;
+  }
+  & .rspv{
+    background: url(/public/aliacas.png) no-repeat center center/cover;
+  }
+  & .regitry{
+    background: url(/public/book.png) no-repeat center center/cover;
+  }
+}
+.registry{
+  background: url(/public/flores.png ) no-repeat center center/cover;
+  min-height: 800px;
+  align-items: center;
+  display: flex ;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  & h3{
+    color: #fff;
+    text-shadow: 0 0 30px #ffffffaf;
+    font-size: 45px;
+    font-weight: normal;
+  }
+  & p{
+
+    color: #fff;
+    text-shadow: 0 0 30px #ffffffaf;
+    font-size: 45px;
+    width: 70%;
+    
   }
 }
 </style>
