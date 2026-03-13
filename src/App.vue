7712eca89d5daac36674724d7389527dae6cd43b
saraSchulz <sara.ifc2024@gmail.com>
2026-03-13 09:29:09 -0300

    keF pagina

--- src/App.vue
+++ src/App.vue
@@ -3,58 +3,79 @@
 </script>
 
 <template>
-<header>
-  <h1 class="logo">
-    <img src="/public/logo.png" alt="logo">
-  </h1>
-  <nav>
-    <ul>
-      <li>Home</li>
-      <li>Our Story</li>
-      <li>Detals</li>
-      <li>RSPV</li>
-    </ul>
-  </nav>
-</header>
-<section class="banner">
-  <h2>
-    Karen<span>&</span>Felipe
-  </h2>
-  <h3>june, 23rd 2026</h3>
-</section>
-<section class="intro">
-  <p>
-    With love and gratitude, we invite you to share in the joy of our wedding day.
-  </p>
-  <p>
-    Tuesday, june, 2026 The Golden Elm Manor, St. Augustine, New York
-  </p>
-</section>
-<section class="cards">
-  <div class="story">
-    <h3>Our Story</h3>
-  </div>
-  <div class="detals">
-    <h3>The Detals</h3>
-  </div>
-  <div class="rspv">
-    <h3>RSPV</h3>
-  </div>
-  <div class="regitry">
+  <header>
+    <h1 class="logo">
+      <img src="/public/logo.png" alt="logo">
+    </h1>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Our Story</li>
+        <li>Detals</li>
+        <li>RSPV</li>
+      </ul>
+    </nav>
+  </header>
+  <section class="banner">
+    <h2>
+      Karen<span>&</span>Felipe
+    </h2>
+    <h3>june, 23rd 2026</h3>
+  </section>
+  <section class="intro">
+    <p>
+      With love and gratitude, we invite you to share in the joy of our wedding day.
+    </p>
+    <p>
+      Tuesday, june, 2026 The Golden Elm Manor, St. Augustine, New York
+    </p>
+  </section>
+  <section class="cards">
+    <a href="#" class="story">
+      <h3>Our Story</h3>
+    </a>
+    <a href="#" class="detals">
+      <h3>The Detals</h3>
+    </a>
+    <a href="#" class="rspv">
+      <h3>RSPV</h3>
+    </a>
+    <a href="#" class="regitry">
+      <h3>Registry</h3>
+    </a>
+
+  </section>
+  <section class="registry">
     <h3>Registry</h3>
-  </div>
-</section>
-<section class="registry">
-  <h3>Registry</h3>
-  <p>
-    Your presence is the most cherished gift. Should you wish to contribute, we've created a honeymoon registry to help us embark on a memorable adventure to the Amalfi Coast.
-  </p>
-  <button>GO to Registry</button>
-</section>
+    <p>
+      Your presence is the most cherished gift. Should you wish to contribute, we've created a honeymoon registry to
+      help us embark on a memorable adventure to the Amalfi Coast.
+    </p>
+    <button>
+      <a href="#">
+        GO to Registry
+      </a>
+    </button>
+  </section>
+  <section class="message">
+    <h3>
+      message
+    </h3>
+    <p>
+      Here you can leave a message, advice, a memory, or just a few special words for us. Each message will be like a
+      little star kept in our history, illuminating this very important moment. We will be very happy to read everything
+      you want to share. Your affection is part of this chapter of our lives.
+    </p>
+    <button>
+      <a href="#">
+      leave message
+      </a>
+    </button>
+  </section>
 </template>
 
 <style scoped>
-header{
+header {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -63,116 +84,198 @@ header{
 
   margin: 0;
   box-shadow: 0 0 75px rgba(0, 0, 0, 0.5);
-  & img{
+
+  & img {
     margin: 20px;
   }
 
-  & ul{
+  & ul {
     display: flex;
     justify-content: center;
     align-items: center;
     margin: 20px;
-  & li{
-    color: #fff;
-    list-style: none;
-    margin: 10px;
-    font-size: 30px;
-    text-decoration: underline 1px solid;
-  }
+
+    & li {
+      color: #fff;
+      list-style: none;
+      margin: 10px;
+      font-size: 30px;
+      text-decoration: underline 1px solid;
+    }
   }
 }
-.banner{
+
+.banner {
   width: 100%;
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-  & h2{
+
+  & h2 {
     color: #fff;
     font-size: 100px;
     font-weight: normal;
     margin: 60px 0 0 0;
-    & span{
-      color:  #FFF94D;
+
+    & span {
+      color: #FFF94D;
     }
   }
-  & h3{
+
+  & h3 {
     color: #fff;
     font-size: 25px;
     font-weight: normal;
     margin: 0 0 60px 0;
   }
 }
-.intro{
+
+.intro {
   background: url(/public/flowers.png ) no-repeat center center/cover;
   min-height: 800px;
   align-items: center;
-  display: flex ;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   text-align: center;
-  & p{
+
+  & p {
     font-size: 45px;
     color: #fff;
     text-shadow: 0 0 30px #ffffffaf;
     width: 50%;
   }
 }
-.cards{
+
+.cards {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
   text-align: center;
-  & div{
+
+  & a {
     width: 35%;
     padding: 300px 0;
     margin: 50px;
     box-shadow: 0 0 75px rgba(0, 0, 0, 0.705);
-     & h3{
+    cursor: pointer;
+    text-decoration: none;
+
+    & h3 {
       color: #fff;
       text-shadow: 0 0 30px #ffffffaf;
       text-decoration: underline 1.5px;
       font-size: 45px;
       font-weight: normal;
-     }
+      transition: all 1s ease;
+    }
+    & h3:hover{
+      color: #000;
+    }
 
   }
-  & .story{
+
+  & .story {
     background: url(/public/folha.png) no-repeat center center/cover;
 
   }
-  & .detals{
+
+  & .detals {
     background: url(/public/carta.png) no-repeat center center/cover;
   }
-  & .rspv{
+
+  & .rspv {
     background: url(/public/aliacas.png) no-repeat center center/cover;
   }
-  & .regitry{
+
+  & .regitry {
     background: url(/public/book.png) no-repeat center center/cover;
   }
 }
-.registry{
+
+.registry {
   background: url(/public/flores.png ) no-repeat center center/cover;
   min-height: 800px;
   align-items: center;
-  display: flex ;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   text-align: center;
-  & h3{
+  margin: 50px 0;
+
+  & h3 {
     color: #fff;
     text-shadow: 0 0 30px #ffffffaf;
     font-size: 45px;
     font-weight: normal;
   }
-  & p{
+
+  & p {
 
     color: #fff;
     text-shadow: 0 0 30px #ffffffaf;
     font-size: 45px;
-    width: 70%;
-    
+    width: 65%;
+
+  }
+  & button{
+    background-color: #ff590083;
+    border: 5px solid #fff;
+    color: #fff;
+    font-family: "Marcellus SC", serif;
+    font-size: 30px;
+    border-radius: 40px;
+    padding: 10px 100px;
+    cursor: pointer;
+    transition: all 0.7s ease;
+  }
+  & button:hover{
+    background-color: #ffffff81;
+    color: #000;
   }
 }
+.message {
+  background: url(/public/mesage.png ) no-repeat center center/cover;
+  min-height: 800px;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  margin: 100px 0;
+
+  & h3 {
+    color: #fff;
+    text-shadow: 0 0 30px #ffffffaf;
+    font-size: 45px;
+    font-weight: normal;
+  }
+
+  & p {
+
+    color: #fff;
+    text-shadow: 0 0 30px #ffffffaf;
+    font-size: 45px;
+    width: 65%;
+
+  }
+  & button{
+    background-color: #ff590083;
+    border: 5px solid #fff;
+    color: #fff;
+    font-family: "Marcellus SC", serif;
+    font-size: 30px;
+    border-radius: 40px;
+    padding: 10px 100px;
+    cursor: pointer;
+    transition: all 0.7s ease;
+  }
+  & button:hover{
+    background-color: #ffffff81;
+    color: #000;
+  }
+}
+
 </style>
